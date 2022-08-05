3c975f0b6d5cf4642e76968576ee7cba1723707b
careyxi <careyxi@zego.im>
2022-08-05 16:19:13 +0800

    👽: update createEngine api.

--- ZegoEasyExample/ZegoEasyExample/AppCenter/AppCenter.swift
+++ ZegoEasyExample/ZegoEasyExample/AppCenter/AppCenter.swift
@@ -10,10 +10,13 @@ import Foundation
 struct AppCenter {
     
     // Get your AppID from ZEGOCLOUD Console
-    static let appID : UInt32 = <#appID#>
+    static let appID : UInt32 = 123
     //[My Projects -> AppID] : https://console.zegocloud.com/project
     
+    // Get your AppID from ZEGOCLOUD Console
+    static let appSign: String = "213"
+    
     // Get your ServerSecret from ZEGOCLOUD Console
-    static let serverSecret: String = "<#serverSecret#>"
+    static let serverSecret: String = "1231"
     // [My Projects -> project's Edit -> Basic Configurations -> ServerSecret] : https://console.zegocloud.com/project"
 }
--- ZegoEasyExample/ZegoEasyExample/ViewController.swift
+++ ZegoEasyExample/ZegoEasyExample/ViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import ZegoExpressEngine
-import ZegoToken
 
 class ViewController: UIViewController {
     
@@ -35,9 +34,8 @@ class ViewController: UIViewController {
         let roomID = roomIDTextField.text ?? ""
         let userID = userIDTextField.text ?? ""
         let user = ZegoUser(userID:userID, userName:("\(userID)Test"))
-        let token = generateToken(userID: user.userID)
         let option: ZegoMediaOptions = [.autoPlayAudio, .publishLocalAudio]
-        ZegoExpressManager.shared.joinRoom(roomID: roomID, user: user, token: token, options: option)
+        ZegoExpressManager.shared.joinRoom(roomID: roomID, user: user, options: option)
         
         presentVideoVC()
     }
@@ -50,12 +48,6 @@ class ViewController: UIViewController {
         // set handler
         ZegoExpressManager.shared.handler = callVC
         self.present(callVC, animated: true, completion: nil)
-    }
-    
-    // !!! When your app is ready to go live, remember not to generate the Token on your client; Otherwise, there is a risk of the ServerSecret being exposed!!!
-    func generateToken(userID: String) -> String {
-        let tokenResult = ZegoToken.generate(AppCenter.appID, userID: userID, secret: AppCenter.serverSecret)
-        return tokenResult.token
     }
 }
 
